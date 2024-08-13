b6a83e40d123594cd7f6c59f52e425b86c58f9e3
Jacintha <129231721+JacinthaDev@users.noreply.github.com>
2024-08-12 21:08:02 -0400

    Update create method in subscriptions_controller.rb

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -27,8 +27,8 @@ class SubscriptionsController < ApplicationController
 
   # POST /subscriptions or /subscriptions.json
   def create
-    @forum = Forum.find(params[:forum_id])
     @subscription = @user.subscriptions.new(subscription_params) # change
+    @subscription.forum_id = @forum.id 
 
     respond_to do |format|
       if @subscription.save
